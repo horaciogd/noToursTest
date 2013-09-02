e83e3689c706e67d406d991ef2e38b4f99156918
Horacio González Diéguez <horaciogd@vhplab.net>
2013-09-02 09:49:02 +0200

    It doese'nt seam to work!

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
-/* Use git exclude in your local repository to avoid having to rewrite all passwords */
+/* Use git exclude in your local repository to avoid having to rewrite all passwords 
+   It doese'nt seam to work!
+*/
 
 /* DATABASE INFORMATION */
 $db = array(
