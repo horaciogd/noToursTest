a172b6dec38733db3f24ab1cd31fe6d6830394f6
Horacio González Diéguez <horaciogd@vhplab.net>
2013-09-02 10:18:53 +0200

    Back to the begining!

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/* Use git exclude in your local repository to avoid having to rewrite all passwords 
+   It doese'nt seam to work!
+   Lets try .gitignore in the server
+   ooops, nothing for the moment
+   .gitignore againg + git rm --cached
+*/
+
+/* DATABASE INFORMATION */
+$db = array(
+	"type" => "*****",
+	"host" => "*****",
+	"data" => "*****",
+	"user" => "*****",
+	"pass" => "*****"
+);
+
+?>
