5bf7b7912cad0f334942fd942c4cd7b9056fbf40
Horacio González Diéguez <horaciogd@vhplab.net>
2013-09-02 10:25:52 +0200

    Lets try a sample file + a local ignored config

---
+++ config.sample.php
@@ -0,0 +1,13 @@
+<?php
+/* Use git .gitignore in your local repository to avoid sharing your passwords */
+
+/* DATABASE INFORMATION */
+$db = array(
+	"type" => "*****",
+	"host" => "*****",
+	"data" => "*****",
+	"user" => "*****",
+	"pass" => "*****"
+);
+
+?>
