e7805cec2613e00b992dd0343766e541f3dd6fa7
Horacio González Diéguez <horaciogd@vhplab.net>
2013-09-02 09:54:44 +0200

    2nd tryout using .gitignore

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 /* Use git exclude in your local repository to avoid having to rewrite all passwords 
    It doese'nt seam to work!
+   Lets try .gitignore in the server
 */
 
 /* DATABASE INFORMATION */
